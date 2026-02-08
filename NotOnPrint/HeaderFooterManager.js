0cd3e2d6f02ba17802ff02b9cf63c92b3f379ca4
JolyneUwU <kipper.johannes@gmx.de>
2026-02-08 20:23:07 +0100

    Add files via upload

---
+++ NotOnPrint/HeaderFooterManager.js
@@ -0,0 +1,54 @@
+class SpeciealHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <header>
+        <h1>Not on Print.com</h1> 
+        <div class="headermenu">
+            <div class="headerbutton">
+            <button class="dropbtn" onclick="window.location.href = '/index.html';">⌂</button>
+            </div> 
+            
+            <div class="dropdown">
+            <button class="dropbtn" onclick="window.location.href = '/text.html';">Text</button>
+                <div class="dropdown-content">
+                    <a href="/Text/TheReadStream.html">The Red Stream</a>
+                    <a href="/Text/Shortstories.html">Shortstories</a>
+                    <a href="/Text/Diary.html">Diary</a>
+                    <a href="/Text/Reviews.html">Reviews</a>
+                    <a href="/de/Text/Places.html">Places</a>
+                </div>
+            </div> 
+
+            <div class="headerbutton">
+                <button class="dropbtn" onclick="window.location.href = '/Audio.html';">Audio</button>
+            </div> 
+
+            <div class="headerbutton">
+                <button class="dropbtn" onclick="window.location.href = '/About.html';">About</button>
+            </div> 
+
+            <div class="headerbutton">
+                <button class="dropbtn" onclick="window.location.href = '/NewestUpload.html';">Latest Texts</button>
+                </div> 
+
+            <div class="dropdown">
+                <button class="dropbtn">Links</button>
+                <div class="dropdown-content">
+                    <a href="https://www.instagram.com/notonprintdotcom?utm_source=qr&igsh=dmh1dXFlYnYzdDFt">Insta</a>
+                    <a href="https://letterboxd.com/kalovedessa/">Letterboxd</a>
+                    <a href="mailto:notonprintcontact@gmail.com">Email</a>
+                    <a href="https://mastodon.social/@Notonprint">Mastodon</a>
+                </div>
+            </div>
+
+            <div class="headerbutton">
+            <button class="dropbtn" onclick="window.location.href = '/de/index.html';">&#127465;&#127466;</button>
+            </div> 
+
+        </div>
+        </header>
+        `
+    }
+}
+
+customElements.define(`special-header`, SpeciealHeader)
---
+++ NotOnPrint/de/HeaderFooterManager.js
@@ -0,0 +1,54 @@
+class SpeciealHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <header>
+        <h1>Not on Print.com</h1> 
+        <div class="headermenu">
+            <div class="headerbutton">
+            <button class="dropbtn" onclick="window.location.href = '/de/index.html';">⌂</button>
+            </div> 
+            
+            <div class="dropdown">
+            <button class="dropbtn" onclick="window.location.href = '/de/text.html';">Text</button>
+                <div class="dropdown-content">
+                    <a href="/Text/TheReadStream.html">The Red Stream</a>
+                    <a href="/de/Text/Shortstories.html">Kurzgeschichten</a>
+                    <a href="/de/Text/Diary.html">Tagebuch</a>
+                    <a href="/Text/Reviews.html">Kritiken</a>
+                    <a href="/de/Text/Places.html">Places</a>
+                </div>
+            </div> 
+
+            <div class="headerbutton">
+                <button class="dropbtn" onclick="window.location.href = '/de/Audio.html';">Audio</button>
+            </div> 
+
+            <div class="headerbutton">
+                <button class="dropbtn" onclick="window.location.href = '/de/About.html';">Über</button>
+            </div> 
+
+            <div class="headerbutton">
+                <button class="dropbtn" onclick="window.location.href = '/de/NewestUpload.html';">Neuste Texte</button>
+                </div> 
+
+            <div class="dropdown">
+                <button class="dropbtn">Links</button>
+                <div class="dropdown-content">
+                    <a href="https://www.instagram.com/notonprintdotcom?utm_source=qr&igsh=dmh1dXFlYnYzdDFt" target="_blank">Insta</a>
+                    <a href="https://letterboxd.com/kalovedessa/" target="_blank">Letterboxd</a>
+                    <a href="mailto:notonprintcontact@gmail.com" target="_blank">Email</a>
+                    <a href="https://mastodon.social/@Notonprint" target="_blank">Mastodon</a>
+                </div>
+            </div>
+
+            <div class="headerbutton">
+            <button class="dropbtn" onclick="window.location.href = '/index.html';">&#127466;&#127475;</button>
+            </div>
+
+        </div>
+    </header>
+        `
+    }
+}
+
+customElements.define(`special-header`, SpeciealHeader)
